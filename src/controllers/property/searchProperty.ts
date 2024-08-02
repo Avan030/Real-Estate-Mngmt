1692caf372cbf82884a18266e567bbaa12a6a2e5
Avan030 <avantikasadhukhan@gmail.com>
2024-08-02 19:12:43 +0530

    Update searchProperty.ts

--- src/controllers/property/searchProperty.ts
+++ src/controllers/property/searchProperty.ts
@@ -4,21 +4,10 @@ import { propertyService } from '@services/index';
 // Controller to search properties
 export const searchProperty = async (req: Request, res: Response): Promise<void> => {
     try {
-        // Extract query parameters from req.query
-        const filters = req.query;
-
-        // Call the service to search properties with the provided filters
-        const properties = await propertyService.searchProperties(filters);
-
-        // Check if properties are found
-        if (!properties || properties.length === 0) {
-            res.status(404).json({ message: 'No properties found' });
-            return;
-        }
-
-        // Send the properties as the response
-        res.json(properties);
+      const { price, size } = req.query as { price?: number; size?: number };
+      const allMusic = await propertyService.searchProperties(price, size);
+      res.status(200).json(allMusic);
     } catch (error) {
-        res.status(500).json({ message: 'Server error' });
+      res.status(500).json({ message: 'Internal server error' });
     }
-};
+  };
